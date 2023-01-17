76e9fe6580f891cad0f738cb77308b89eab15539
divakar10d <divakar10d@gmail.com>
2023-01-17 23:18:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { observer, useLocalObservable } from 'mobx-react-lite'
+import { useEffect } from 'react'
+import Guess from "./component/Guess"
+import Qwerty from './component/Qwerty'
+import PuzzleStore from './stores/PuzzleStore'
 
-function App() {
+export default observer(function App() {
+  const store = useLocalObservable(() => PuzzleStore)
+  useEffect(() => {
+    store.init()
+    window.addEventListener('keyup', store.handleKeyup)
+
+    return () => {
+      window.removeEventListener('keyup', store.handleKeyup)
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
+      <h1 className="bg-gradient-to-br from-blue-400 to-green-400 bg-clip-text text-6xl font-bold uppercase text-transparent">
+        Wordle
+      </h1>
+      {store.guesses.map((_, i) => (
+        <Guess
+          key={i}
+          word={store.word}
+          guess={store.guesses[i]}
+          isGuessed={i < store.currentGuess}
+        />
+      ))}
+      {store.won && <h1>You won!</h1>}
+      {store.lost && <h1>You lost!</h1>}
+      {(store.won || store.lost) && (
+        <button onClick={store.init}>Play Again</button>
+      )}
+      <Qwerty store={store} />
     </div>
-  );
-}
-
-export default App;
+  )
+})
